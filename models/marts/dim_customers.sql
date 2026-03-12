ae3aed5f131436504cb7643673b7e034b96ed385
yeuseyenka <eugene.yeuseyenka@gmail.com>
2026-03-12 12:09:40 +0400

    some changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -2,7 +2,7 @@ with customers as (
     select * from {{ ref ('stg_jaffle_shop__customers')}}
 ),
 orders as (
-    select * from {{ ref ('fct_orders')}}
+    select * from {{ ref ('stg_jaffle_shop__orders')}}
 ),
 customer_orders as (
     select
